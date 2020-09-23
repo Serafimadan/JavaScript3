b24a616c013563df51e28afc6f00972e6b622031
serafima <svp1982@mail.ru>
2020-09-23 23:13:16 +0200

    Separate functions in the project in different files

---
+++ hackyourrepo-app/hackyourrepo-app/util/fetchData.js
@@ -0,0 +1,71 @@
+'use strict';
+import {
+  selectElement,
+  aElem,
+  repoDescription,
+  repoForks,
+  repoUpdated,
+  secondSection,
+  thirdSection,
+  mainBlock,
+  containerContributor,
+} from './domElem.js';
+
+export function fetchData() {
+   const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+   fetch(url)
+     .then(res => res.json())
+     .then(repositoriesList => {
+       // sort array names in select by alphabet
+       repositoriesList.sort((a, b) => {
+         const fistName = a.name.toLowerCase();
+         const nextName = b.name.toLowerCase();
+         if (fistName < nextName) return -1;
+         if (fistName > nextName) return 1;
+       });
+       repositoriesList.forEach(repository => {
+         // getting names for options in the select
+         const option = document.createElement('option');
+         option.innerText = repository.name;
+         selectElement.appendChild(option);
+         // getting information about repository
+         if (selectElement.value === repository.name) {
+           aElem.textContent = repository.name;
+           repoDescription.textContent = repository.description;
+           repoForks.textContent = repository.forks;
+           repoUpdated.textContent = repository.updated_at;
+           // get information about contributors
+           const contributionUrl = repository.contributors_url;
+           fetch(contributionUrl)
+             .then(res => {
+               return res.json();
+             })
+             .then(data => {
+               let cardElement = '';
+               data.forEach(contributor => {
+                 cardElement += `<div class='card person'>
+                  <img src= ${contributor.avatar_url} alt = ${contributor.login} class="contribPict"/> 
+                  <a class='name'>${contributor.login}</a> 
+                  <div class='badge'>${contributor.contributions}</div>
+                  </div>`;
+                 // let notesOnPage = 5;
+                 // let countOfItems = Math.ceil(contributor.length / notesOnPage);
+               });
+               containerContributor.innerHTML = cardElement;
+             });
+         }
+       });
+     })
+     .catch(() => {
+       selectElement.innerHTML = '';
+       secondSection.style.display = 'none';
+       thirdSection.style.display = 'none';
+       mainBlock.style.backgroundColor = '#f8d7d9';
+       mainBlock.style.padding = '1.2rem';
+       mainBlock.style.marginTop = '0.2rem';
+       const errorText = document.createElement('p');
+       errorText.style.color = '#803438';
+       errorText.innerText = 'Network request failed';
+       mainBlock.appendChild(errorText);
+     });
+ }
